d42e972d08d32b4fa9ac7f5856acb97105c5cd9b
Jorge Pino Vara <jpv@spyro.es>
2025-05-20 15:18:09 +0200

    Agregamos cosas

--- obj/Debug/net9.0/VoskRealtimeApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/VoskRealtimeApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VoskRealtimeApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a15ae104f7d5f160874f20d2a178e623b3ee0b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e5e0dbed4e8c8fc47873eba97ddbbe9da7a5dbd")]
 [assembly: System.Reflection.AssemblyProductAttribute("VoskRealtimeApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VoskRealtimeApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
